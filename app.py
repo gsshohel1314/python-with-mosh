ed673941378cf47bebb0dc7284249924ae399e77
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-21 00:21:55 +0600

    formatted strings

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
-course = "python's course for beginners"
-print(course)
-
-new_course = '''
-hi shohel,
-
-here is our first email to you,
-
-thank you.
-The support team       
-'''
-print(new_course)
-
-string_index = "Python for beginners"
-print(string_index[0])
-print(string_index[-2])
-print(string_index[0:3])
-print(string_index[0:])
-print(string_index[1:])
-print(string_index[:5])
-another = string_index[:]
-print(another)
-name = "Jennifer"
-print(name[1:-1])
-
+first = "John"
+last = 'Smith'
+name = first +' '+ last +' is a coder'
+print(name)
+name_one = first + ' [' + last + '] is a coder'
+print(name_one)
+name_two = f'{first} [{last}] is a coder' #best practice
+print(name_two)
 
