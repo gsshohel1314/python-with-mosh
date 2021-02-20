ba5b677ce6007e2d92533ae957e77e37cb58188f
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-20 18:26:27 +0600

    variable in python

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-print("Shohel Rana")
-print('0----')
-print(' ____')
-print('*' * 10)
+price = 10
+price = 20
+print(price)
+rating = 5.5
+print(rating)
+name = "shohel"
+print(name)
+is_published = False
+print(is_published)
