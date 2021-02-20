22a4d6f68eba69429486558a62fe8266d02dbf0b
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-20 19:02:18 +0600

    practice type conversion in python

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-name = input('What is your name? :')
-color = input('What is your favorite color? :')
-print ("Hi " + name + " Your favorite color " +color)
+birth_year = input('Input your birth year:')
+print(type(birth_year))
+age = 2021 - int(birth_year)
+print(type(age))
+print(age)
+
+weight_lbs = input('input your weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
 
