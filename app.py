df3d71a6636cf9bf7773c87cf6a052f9c6d18588
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-21 10:12:40 +0600

    arithmetic operators and argumented assignment operators

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-course = 'Python for beginners'
-print(len(course))
-print(course.upper())
-print(course.lower())
-print(course.find('P'))
-print(course.find('beginners')) #find method is case sensetive
-print(course.replace('beginners', 'Absolute Beginners'))
-print('Python' in course)
+print(10 + 3) #addition
+print(10 - 3) #subtraction
+print(10 * 3) #multiplication
+print(10 / 3) #divition
+print(10 // 3) #divition for integer value
+print(10 % 3) #modulus or vagshes
+print(10 ** 3) #power
+
+print("Argumented assignment operator")
+x = 10
+x= x + 3
+print(x)
+
+x = 10
+x = x - 3
+print(x)
+
+print("Argumented assignment operator short form")
+x = 10
+x += 3 #that means x = x + 3
+print(x)
+
+x = 10
+x -= 3 #that means x = x - 3
+print(x)
 
