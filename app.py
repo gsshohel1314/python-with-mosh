c401549a51ceae110d6567b292d442e3e1f7bbbb
gs shohel1314 <gsshohel1314@gmail.com>
2021-02-21 10:27:44 +0600

    Operator Precedence in python

--- app.py
+++ app.py
@@ -1,26 +1,13 @@
-print(10 + 3) #addition
-print(10 - 3) #subtraction
-print(10 * 3) #multiplication
-print(10 / 3) #divition
-print(10 // 3) #divition for integer value
-print(10 % 3) #modulus or vagshes
-print(10 ** 3) #power
+print("Operator Precedence:")
+# parenthesis > exponentiation > multiplication or divition > addition or subtraction
 
-print("Argumented assignment operator")
-x = 10
-x= x + 3
+x = (10 + 3) * 2 ** 2
+print(x)
+x = 10 + 3 * 2 ** 2
+print(x)
+x = 10 + 3 * 2 - 2
 print(x)
 
-x = 10
-x = x - 3
+print("Exercise:")
+x = (2 + 3) * 10 - 3
 print(x)
-
-print("Argumented assignment operator short form")
-x = 10
-x += 3 #that means x = x + 3
-print(x)
-
-x = 10
-x -= 3 #that means x = x - 3
-print(x)
-
